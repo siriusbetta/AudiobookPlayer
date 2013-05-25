9868fadd8223bb750cf3db933836a632a0593b82
siriusbetta <aleksey.konyshev@gmail.com>
2013-05-25 21:47:34 +0800

    added class Playlist data model of playlist

--- src/web/audiobooks/models/Playlist.java
+++ src/web/audiobooks/models/Playlist.java
@@ -1,5 +1,51 @@
 package web.audiobooks.models;
 
-public class Playlist {
+import java.util.Date;
 
+/**
+ * <p>Data model of user's playlist</p>
+ * @author Aleksey
+ *
+ */
+public class Playlist {
+	
+	/**
+	 * @value Integer playlistID;
+	 */
+	private int playlistID;
+	
+	/**
+	 * @value Integer userID
+	 */
+	private int userID;
+	/**
+	 * <p>List of user's audio </p>
+	 * @value List<Audio> audio 
+	 */
+	
+	/**
+	 * <p>Time of last modification</p>
+	 * @value Date timeOfModification
+	 */
+	private Date timeOfModification;
+	
+	public int getPlaylistID() {
+		return playlistID;
+	}
+	public void setPlaylistID(int playlistID) {
+		this.playlistID = playlistID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public Date getTimeOfModification() {
+		return timeOfModification;
+	}
+	public void setTimeOfModification(Date timeOfModification) {
+		this.timeOfModification = timeOfModification;
+	}
+	
 }
