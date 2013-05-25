b2dd34311ca49781c37a923d0b9d76a05fd9dc99
siriusbetta <aleksey.konyshev@gmail.com>
2013-05-25 17:52:35 +0800

    add method mapperRow to AudioMapper

--- src/web/audiobooks/data/mapper/AudioMapper.java
+++ src/web/audiobooks/data/mapper/AudioMapper.java
@@ -1,15 +1,56 @@
 package web.audiobooks.data.mapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import web.audiobooks.models.Audio;
+
 
 /**
  * 
  * @author Aleksey
  * @category Mapper
  * @see Audio
- * Class returns object Audio from Data Base
+ * <p>
+ * 	Class returns object Audio from Data Base
+ * </p>
  *
  */
 public class AudioMapper {
-	
+	/**
+	 * @param ResultSet resultSet, Integer rowNumber
+	 * <p>
+	 * 	Object formed from audio's table in database
+	 * </p>
+	 * @return Audio audio 
+	 * @exception SQLException If get exception return NULL
+	 * 
+	 */
+	public Audio mapperRow(ResultSet resultSet, Integer rowNumber){
+	/**
+	 * <p>Creates new object Audio which mapping from table</p>
+	 * @value Audio audio 
+	 */
+		Audio audio = new Audio();
+		
+		try{
+			
+			audio.setAudioID(resultSet.getInt("audioID"));
+			
+			audio.setAuthor(resultSet.getString("author"));
+			
+			audio.setBookName(resultSet.getString("bookName"));
+			
+			audio.setLength(resultSet.getDouble("length"));
+			
+			audio.setPath(resultSet.getString("path"));
+			
+		}catch(SQLException e){
+			
+			return null;
+		}
+		
+		return audio;	
+	}
 
 }
