e32422783ed984cd23fbcb6f2303a18b13cc4855
siriusbetta <aleksey.konyshev@gmail.com>
2013-05-25 17:32:49 +0800

    Create class AudioMapper

---
+++ src/web/audiobooks/data/mapper/AudioMapper.java
@@ -0,0 +1,15 @@
+package web.audiobooks.data.mapper;
+
+
+/**
+ * 
+ * @author Aleksey
+ * @category Mapper
+ * @see Audio
+ * Class returns object Audio from Data Base
+ *
+ */
+public class AudioMapper {
+	
+
+}
