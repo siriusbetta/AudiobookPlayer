7ab69ef0c980b58be84bb25f4e111586305c1058
siriusbetta <aleksey.konyshev@gmail.com>
2013-05-30 01:22:17 +0800

    worked with sql's tested searching by words in phrases

--- WebContent/sql/audio.sql
+++ WebContent/sql/audio.sql
@@ -1,2 +1,2 @@
-create table audio(audioID int unsigned auto_increment primary key, path text, author text, length double, 
-bookName text, keyWords) auto_increment = 1;
+create table audio(audioID int unsigned auto_increment primary key, path text, author varchar(200), length double, 
+bookName text, keyWords text) auto_increment = 1;
--- WebContent/sql/query.sql
+++ WebContent/sql/query.sql
@@ -1,3 +1,24 @@
 delete from audio where author = 'Linda';
 
-select * from audio;
+select keyWords from audio where author = 'Arabika';
+
+select keyWords from audio where match (author, keyWords) against ('psih');
+
+create fulltext index findtext on audio ( author, keyWords);
+
+create database audioplayer;
+create table test(id int unsigned auto_increment primary key, name varchar(200), body text,
+fulltext (name, body)) auto_increment = 1;
+
+insert into test (name, body) values ('Alex', 'capitan, has very dificult cheracter. Likes women and spend time with friends');
+
+insert into test (name, body) values ('Oleg', 'leutenenant, has very dificult cheracter. Drinks a lot of.');
+
+insert into test (name, body) values ('Misha', 'capral, chearful. Likes women and smoke drugs');
+
+select * from test where match (name, body) against ("oleg");
+select * from test where body like '%drugs%';
+
+
+SELECT * FROM audio WHERE keyWords REGEXP 'new age|psih';
+select * from audio where keyWords like '%new age%';
