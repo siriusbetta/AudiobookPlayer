a6d78e413699ca515bca0f43e05b575c71158661
siriusbetta <aleksey.konyshev@gmail.com>
2013-05-31 02:00:40 +0800

    added comment to SearchControllr

--- src/web/audiobooks/controller/SearchController.java
+++ src/web/audiobooks/controller/SearchController.java
@@ -1,5 +1,13 @@
 package web.audiobooks.controller;
 
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>The class processes search queries by books name, authors, series</p>
+ * @author Aleksey
+ *
+ */
+@Controller
 public class SearchController {
 
 }
