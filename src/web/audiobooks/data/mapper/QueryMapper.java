5c9fd243b1df9126eb6665d83d5ebdbe4e2ab6ed
siriusbetta <aleksey.konyshev@gmail.com>
2013-06-04 00:52:44 +0800

    added mapper QueryMapper

---
+++ src/web/audiobooks/data/mapper/QueryMapper.java
@@ -0,0 +1,10 @@
+package web.audiobooks.data.mapper;
+/**
+ * <p>Mapper for queries but the mapper is unnecessary for this project</p>
+ *  <p>Query uses just like parameter, not like result from database</p>
+ * @author Aleksey
+ *
+ */
+public class QueryMapper {
+
+}
---
+++ src/web/audiobooks/models/Query.java
@@ -0,0 +1,21 @@
+package web.audiobooks.models;
+/**
+ * <p>The model of query</p>
+ * @author Aleksey
+ * 
+ */
+public class Query {
+	/**
+	 * <p>Query</p>
+	 */
+	private String query;
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+}
