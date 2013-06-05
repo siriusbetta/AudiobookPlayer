277d3c88e76bf437a8e97f19b874a59fccc66e3b
siriusbetta <aleksey.konyshev@gmail.com>
2013-06-05 14:13:11 +0800

    wrote method to QueryController getQuery

--- src/web/audiobooks/controller/SearchController.java
+++ src/web/audiobooks/controller/SearchController.java
@@ -1,6 +1,10 @@
 package web.audiobooks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import web.audiobooks.models.Query;
 
 /**
  * <p>The class processes search queries by books name, authors, series</p>
@@ -9,5 +13,10 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class SearchController {
-
+	@RequestMapping("/getQuery.htm")
+	public String getQuery(ModelMap model){
+		Query query = new Query();
+		model.addAttribute("query", query);
+		return "main";
+	}
 }
