70ec78a0b1363557504b2d544739fae51894e5e0
siriusbetta <aleksey.konyshev@gmail.com>
2013-06-05 14:35:19 +0800

    wrote new method to QueryFactory

--- src/web/audiobooks/data/factory/QueryFactory.java
+++ src/web/audiobooks/data/factory/QueryFactory.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import web.audiobooks.data.template.AudioTemplate;
+import web.audiobooks.data.template.QueryTemplate;
 
 
 /**
@@ -20,9 +21,19 @@ public class QueryFactory {
 	 * <p>Return audio template with queries</p>
 	 * @return AudioTemplate audioTemplate 
 	 */
-	public AudioTemplate queryBag(){
+	public AudioTemplate queryAudio(){
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		AudioTemplate audioTemplate = (AudioTemplate)context.getBean("audioTemplate");
 		return audioTemplate;
+	}
+	
+	/**
+	 * <p>Return query template with queries</p>
+	 * @return QueryTemplate queryTemplate 
+	 */
+	public QueryTemplate querySearch(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		QueryTemplate queryTemplate = (QueryTemplate)context.getBean("queryTemplate");
+		return queryTemplate;
 	}
 }
